176bcb9b191427b510c45b8542f19d74e9a6c2fb
swimmingkiim <swimmingkiim@gmail.com>
2020-10-01 14:53:19 +0900

    solve ctx problem, now can store position and draw dots on the canvas

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import * as S from "./style";
-import Action from "page/actions";
 
 interface CanvasProps {
   canvasRef: React.RefObject<HTMLCanvasElement>;
-  dispatch: React.Dispatch<Action>;
   onClickFunc?: (e: React.MouseEvent<HTMLCanvasElement>) => void;
   onDragFunc?: (e: React.MouseEvent<HTMLCanvasElement>) => void;
 }
 
 const Canvas: React.FC<CanvasProps> = ({
   canvasRef,
-  dispatch,
   onClickFunc,
   onDragFunc,
 }) => {
   return (
     <S.CanvasSelf
       ref={canvasRef}
+      width={canvasRef.current?.offsetWidth}
+      height={canvasRef.current?.offsetHeight}
       onClick={onClickFunc}
       onDrag={onDragFunc}
     ></S.CanvasSelf>
--- src/page/page.tsx
+++ src/page/page.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import Template from "./template";
 import Canvas from "components/Canvas/Canvas";
 import reducer from "./reducer";
@@ -11,7 +11,30 @@ const Page = () => {
     []
   );
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const canvas = <Canvas canvasRef={canvasRef} dispatch={dispatch} />;
+  const onCanvasClick = (e: React.MouseEvent<HTMLCanvasElement>): void => {
+    if (canvasRef.current) {
+      const { offsetTop, offsetLeft } = canvasRef.current;
+      const x = e.pageX - offsetLeft;
+      const y = e.pageY - offsetTop;
+      try {
+        dispatch({ type: "ADD_DOT", coordinate: { x, y } });
+      } catch (error) {
+        console.log(String(error));
+      }
+    }
+  };
+  const canvas = <Canvas canvasRef={canvasRef} onClickFunc={onCanvasClick} />;
+  const ctx = canvasRef.current?.getContext("2d");
+
+  useEffect(() => {
+    if (Boolean(state.length)) {
+      const { x, y } = state[state.length - 1];
+      ctx?.beginPath();
+      ctx?.arc(x, y, 5, 0, Math.PI * 2, true);
+      ctx?.fill();
+    }
+  }, [state, ctx]);
+
   return <Template canvas={canvas} />;
 };
 
